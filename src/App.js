f7b93faeb5c716a349db41820a0bda29ca3bad05
Tim <tim@congruentlabs.co>
2022-06-09 21:36:30 +1000

    dep updates, fixing eslint for vscode

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { DAppProvider } from '@usedapp/core';
 import { createBrowserHistory } from 'history';
 import Routes from './Routes';
 import Page from './components/Page';
 
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import 'aos/dist/aos.css';
 
 import 'scss/react-images.scss';
-import 'scss/slick-slider.scss';
 
 const browserHistory = createBrowserHistory();
 
 const App = () => {
   return (
-    <DAppProvider>
-      <Page>
-        <Router history={browserHistory}>
-          <Routes />
-        </Router>
-      </Page>
-    </DAppProvider>
+    <Page>
+      <Router history={browserHistory}>
+        <Routes />
+      </Router>
+    </Page>
   );
 };
 
