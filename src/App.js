7d0f04de617c5a7a9ade2757fdc672ef6fa71961
Tim <tim@congruentlabs.co>
2022-06-11 08:52:41 +1000

    removing broken routing for now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import Routes from './Routes';
+// import { Router } from 'react-router-dom';
+// import { createBrowserHistory } from 'history';
+// import Routes from './Routes';
 import Page from './components/Page';
+import { IndexView } from 'views';
 
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import 'aos/dist/aos.css';
 
 import 'scss/react-images.scss';
 
-const browserHistory = createBrowserHistory();
+// const browserHistory = createBrowserHistory();
 
 const App = () => {
   return (
     <Page>
-      <Router history={browserHistory}>
-        <Routes />
-      </Router>
+      <IndexView />
     </Page>
   );
 };
